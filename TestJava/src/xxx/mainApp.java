5ba4fbc56270d1d15a0c2617d322e0e926436544
Romin <ilove52345234@gmail.com>
2021-06-08 16:09:15 +0800

    gakki

--- TestJava/src/xxx/mainApp.java
+++ TestJava/src/xxx/mainApp.java
@@ -4,6 +4,17 @@ public class mainApp {
 	
 	public static void main(String[] args) {
        System.out.println("第一個版本");
+       int a=new gakki().add(10, 20);
+       
+       System.out.println(a);
     }
 
+}
+
+class gakki{
+	
+	int add(int a,int b) {
+		return a+b;
+	}
+	
 }
